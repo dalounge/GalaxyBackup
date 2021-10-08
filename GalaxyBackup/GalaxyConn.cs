fdb1fe54732488d88fc2fe9c2ded7adf3fb03cd1
Dale Nguyen <daletnguyen@gmail.com>
2021-10-08 14:01:29 -0500

    Initial commit

---
+++ GalaxyBackup/GalaxyConn.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArchestrA.GRAccess;
+
+
+namespace GalaxyBackup
+{
+    class GalaxyConn
+    {
+        private string _GalaxyName { get; set; }
+        private string _UserName { get; set; }
+        private string _Password { get; set; }
+        public IGalaxy Galaxy { get; set; }
+        public GalaxyConn(string galaxyName, string userName, string password)
+        {
+            _GalaxyName = galaxyName;
+            _UserName = userName;
+            _Password = password;
+        }
+
+        public void Connect(GRAccessApp grAccess)
+        {
+
+            string nodeName = Environment.MachineName;
+            Console.WriteLine($"Machine: {nodeName}");
+            Console.WriteLine($"Connecting to galaxy: {_GalaxyName}");
+
+            IGalaxies gals = grAccess.QueryGalaxies(nodeName);
+            if (gals == null || grAccess.CommandResult.Successful == false)
+            {
+                Console.WriteLine(grAccess.CommandResult.CustomMessage + grAccess.CommandResult.Text);
+            }
+
+            IGalaxy galaxy = gals[_GalaxyName];
+
+            ICommandResult cmd;
+
+            galaxy.Login(_UserName, _Password);
+            cmd = galaxy.CommandResult;
+
+            if (!cmd.Successful)
+            {
+                Console.WriteLine($"Login to galaxy {_GalaxyName} Failed :" +
+                                cmd.Text + " : " +
+                                cmd.CustomMessage);
+            }
+            else
+            {
+                Console.WriteLine($"Login {_GalaxyName} successful");
+            }
+
+            Galaxy = galaxy;
+        }
+    }
+}
---
+++ GalaxyBackup/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ArchestrA.GRAccess;
+using System.Diagnostics;
+
+namespace GalaxyBackup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Enter Galaxy Name to Backup: ");
+            string galaxyName = Console.ReadLine();
+
+            Console.WriteLine("Name of backup -- c:/AVEVA/XXXXX.CAB");
+            string backupName = Console.ReadLine();
+            string fullLocation = "C:/AVEVA/" + backupName + ".cab";
+
+            Console.WriteLine("Username: ");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Password: ");
+
+            string password = null;
+
+            while (true)
+            {
+                ConsoleKeyInfo key = System.Console.ReadKey(true);
+                if (key.Key == ConsoleKey.Enter)
+                    break;
+                password += key.KeyChar;
+            }
+
+            Console.WriteLine("");
+
+            GRAccessApp grAccess = new GRAccessApp();
+            GalaxyConn gConn = new GalaxyConn(galaxyName, userName, password);
+
+            gConn.Connect(grAccess);
+            int processId = Process.GetProcessesByName("aaGR")[0].Id;
+
+            Console.WriteLine();
+            Console.WriteLine("Backing up galaxy");
+
+            gConn.Galaxy.Backup(processId, fullLocation, Environment.MachineName, galaxyName);
+
+            Console.WriteLine("");
+            Console.WriteLine("FINISHED");
+            Console.ReadLine();
+        }
+    }
+}
